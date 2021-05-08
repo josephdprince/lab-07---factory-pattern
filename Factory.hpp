30c2bb325c934805a66ed72341264d8383e29ac4
puloma-k <puloma@pulomakatiyar.com>
2021-05-08 16:19:13 -0700

    Fixed error checking in parse function

--- Factory.hpp
+++ Factory.hpp
@@ -3,37 +3,39 @@
 
 #include <string>
 #include "base.hpp"
+#include "op.hpp"
 
 class Factory {
 	public:
 		Base* parse(char** input, int length) {
-			Base* prev = null;
-			string exp = input[1];
-			if((exp.at(0).isdigit() == 0) || exp.at(length - 1).isdigit() == 0)
-				return null;
-			for(unsigned i = 0; i < length-1; i++) {
+			if(length < 2)
+				return nullptr;
+			Base* prev = nullptr;
+			std::string exp = input[1];
+			int expSize = exp.size();
+			if((isdigit(exp.at(0)) == 0) || isdigit(exp.at(expSize-1)) == 0)
+				return nullptr;
+			for(unsigned i = 0; i < expSize-1; i++) {
 				char cur = exp.at(i);
 				char next = exp.at(i+1);
-				if((cur.isdigit() == 0) ||
-					(cur != "+") || (cur != "-") ||
-					(cur != "*") || (cur != "/")) {
-					return null;
+				if((isdigit(cur) == 0) && (cur != '+') && (cur != '-') && (cur != '*') && (cur != '/')) {
+					return nullptr;
 				}
-				if((cur == "+") || (cur == "/") || (cur == "-")) {
-					if(next.isdigit() == 0)
-						return null;
+				if((cur == '+') || (cur == '/') || (cur == '-')) {
+					if(isdigit(next) == 0)
+						return nullptr;
 				}
-				if(cur == "*") {
-					if((next.isdigit() == 0) || (next != "*"))
-						return null;
-					if(next == "*") {
-						if(exp.at(i+2) == "*")
-							return null;
+				if(cur == '*') {
+					if((isdigit(next) == 0) && (next != '*'))
+						return nullptr;
+					if(next == '*') {
+						if(exp.at(i+2) == '*')
+							return nullptr;
 					}
 				} 
 			}
 			/*
-			for(unsigned i = 0; i < length; i+=2) {
+			for(unsigned i = 0; i < expSize; i+=2) {
 				if(exp.at(i).isdigit() != 0) {
 				
 				} else {
